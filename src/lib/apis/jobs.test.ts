804af5e3427e99c405c0213b02492bc989c6cb27
athomas <18246773+chronark@users.noreply.github.com>
2020-03-16 12:37:19 +0100

    Dev (#148)
    * added lockFileMaintenance
    
    * downloading rawJobs to static dir
    
    * jobs.get() has tests for empty jobs and a single job
    
    * Update dependency eslint-plugin-jest to v23.8.2
    
    * Update dependency @types/jest to v25.1.4
    
    * Update dependency @docusaurus/core to v2.0.0-alpha.44
    
    * Update dependency @docusaurus/core to v2.0.0-alpha.47
    
    * Update dependency @docusaurus/core to v2.0.0-alpha.48
    
    * Update dependency @docusaurus/preset-classic to v2.0.0-alpha.48
    
    * Update typescript-eslint monorepo to v2.23.0
    
    * Update dependency start-server-and-test to v1.10.9
    
    * Update dependency eslint-plugin-jsdoc to v22.0.1
    
    * Update dependency regenerator-runtime to v0.13.5
    
    * Update dependency @types/node to v12.12.30
    
    * Update dependency start-server-and-test to v1.10.10
    
    * Update dependency typedoc to v0.17.0
    
    * Lock file maintenance (#167)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    
    * Update dependency eslint-plugin-unicorn to v17 (#151)
    
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: athomas <18246773+chronark@users.noreply.github.com>
    
    Co-authored-by: Andreas Thomas <andreas.thomas@chronark.com>
    Co-authored-by: Renovate Bot <bot@renovateapp.com>
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

---
+++ src/lib/apis/jobs.test.ts
@@ -0,0 +1,81 @@
+import { Job, RawSearch } from "../../types/customTypes"
+
+import { Jobs } from "./jobs"
+
+describe("jobs", () => {
+  describe("get()", () => {
+    interface TestCase {
+      name: string
+      rawSearch: RawSearch
+      want: Job[]
+    }
+
+    const tt: TestCase[] = [
+      {
+        name: "should return an empty array",
+        rawSearch: {
+          jobs: [],
+          orte: [],
+        },
+        want: [],
+      },
+      {
+        name: "should return a single job",
+        rawSearch: {
+          jobs: [
+            {
+              ID: "631520",
+              datum: "03/05/2020",
+              titel: "Werkstudent / Praktikant (w/m/d) im technischen Vertrieb",
+              lat: "48.135125299999999",
+              lng: "11.5819805",
+              typ: "Internship while studying",
+              extern: "null",
+              logo: "",
+              firma: "Innoscripta GmbH",
+              url:
+                "https://jobboerse.th-nuernberg.de/index.php/en/job/631520-Werkstudent-Praktikant-wmd-im-technischen-Vertrieb-Innoscripta-GmbH",
+            },
+          ],
+          orte: [],
+        },
+        want: [
+          {
+            score: 1,
+            id: 631520,
+            date: "03/05/2020",
+            title: "Werkstudent / Praktikant (w/m/d) im technischen Vertrieb",
+            locations: [
+              {
+                lat: 48.135125299999999,
+                lon: 11.5819805,
+              },
+            ],
+            type: "Internship while studying",
+            logo: "",
+            corp: "Innoscripta GmbH",
+            url:
+              "https://jobboerse.th-nuernberg.de/index.php/en/job/631520-Werkstudent-Praktikant-wmd-im-technischen-Vertrieb-Innoscripta-GmbH",
+          },
+        ],
+      },
+    ]
+
+    tt.forEach(tc => {
+      jest.resetAllMocks()
+      jest.spyOn(Math, "random").mockImplementation(() => 1)
+      window.fetch = jest.fn().mockImplementation(() => {
+        return Promise.resolve({
+          ok: true,
+          json: () => tc.rawSearch,
+        })
+      })
+
+      const jobs = new Jobs()
+
+      test(tc.name, () => {
+        expect(jobs.get()).toEqual(Promise.resolve(tc.want))
+      })
+    })
+  })
+})
