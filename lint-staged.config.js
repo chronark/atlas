05d1d62a1cc093ef55a06a4837dfd9a0791c09d8
Andreas Thomas <dev@chronark.com>
2020-05-13 14:20:57 +0200

    Build docs only for changed files (#336)
    Before this every commit was full of changed docs wherea a single id was
    different.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   "**/*.{css,html,json,md,yaml,yml}": ["prettier --write"],
-  "**/*.{ts}": ["prettier --write", "eslint --cache --ext '.js,.ts' --fix"],
+  "**/*.{ts}": [
+    "prettier --write",
+    "eslint --cache --ext '.js,.ts' --fix",
+    "./node_modules/.bin/typedoc ./src/types/ol-mapbox-style.d.ts ./src/types/olext.d.ts",
+  ],
 }
